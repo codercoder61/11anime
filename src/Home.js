603a028af6e81e4876d2df22f003e7a46df93365
codercoder61 <mohamedknoud@gmail.com>
2026-01-17 19:13:26 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -46,7 +46,7 @@ const [searchParams] = useSearchParams();
     }
   
     try {
-      const { data } = await axios.get('https://dollars-approx-perth-str.trycloudflare.com/search', {
+      const { data } = await axios.get('https://low-inherited-cheapest-guitar.trycloudflare.com/search', {
         params: { keyword: search },
       });
       //(data)
@@ -159,7 +159,7 @@ const totalPages = Math.ceil((recUpd?.length || 0) / itemsPerPage);
   if (anchor) anchor.scrollIntoView({ behavior: "smooth" });
 
   try {
-    const response = await axios.get(`https://dollars-approx-perth-str.trycloudflare.com/search?keyword=${show}`);
+    const response = await axios.get(`https://low-inherited-cheapest-guitar.trycloudflare.com/search?keyword=${show}`);
     //('Response:', response);
 
     const newData = response.data.animeList || [];
@@ -178,7 +178,7 @@ const totalPages = Math.ceil((recUpd?.length || 0) / itemsPerPage);
     const fetchData = async (page=1) => {
       try {
          
-        const response = await axios.get(`https://dollars-approx-perth-str.trycloudflare.com/?category=tv&page=${page}`, {
+        const response = await axios.get(`https://low-inherited-cheapest-guitar.trycloudflare.com/?category=tv&page=${page}`, {
       type:'tv',
     })
         //(response)
@@ -195,7 +195,7 @@ const totalPages = Math.ceil((recUpd?.length || 0) / itemsPerPage);
       {
 setLoading2(true)
 
-      axios.get('https://dollars-approx-perth-str.trycloudflare.com/filter', {
+      axios.get('https://low-inherited-cheapest-guitar.trycloudflare.com/filter', {
     params: {
       genree: genree1,
       typee: typee1,
@@ -231,7 +231,7 @@ const handleSubmit = (e) => {
   setRecUpd(null);  // Clear previous filtered results
 
   // Make the API call with the filter parameters as query parameters
-  axios.get('https://dollars-approx-perth-str.trycloudflare.com/filter', {
+  axios.get('https://low-inherited-cheapest-guitar.trycloudflare.com/filter', {
   params: {
     typee,
     statuse,
@@ -392,7 +392,7 @@ const handleMouseEnter = (animeId, index) => {
   setHoveredIndex(index);
 setLoading(true);
   axios
-  .get('https://dollars-approx-perth-str.trycloudflare.com/animeInfo', {
+  .get('https://low-inherited-cheapest-guitar.trycloudflare.com/animeInfo', {
     params: { animeId }
   })
   .then(response => {
